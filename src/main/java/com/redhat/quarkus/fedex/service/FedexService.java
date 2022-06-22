021d8e8ed6ac5d73a25104ac249f8413db26e223
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-06-22 11:14:38 -0300

    delay

--- src/main/java/com/redhat/quarkus/fedex/service/FedexService.java
+++ src/main/java/com/redhat/quarkus/fedex/service/FedexService.java
@@ -10,18 +10,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import com.redhat.quarkus.fedex.domain.Label;
 import com.redhat.quarkus.fedex.domain.Order;
 
 @Path("/fedex")
 public class FedexService {
 
+  @ConfigProperty(name="label.delay", defaultValue = "50")
+  long delay;
+
   @POST
   @Path("/createlabel")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public Label createLabel(Order order) {
+  public Label createLabel(Order order) throws InterruptedException {
     System.out.println("Label generated -> "+UUID.randomUUID().toString());
+    Thread.sleep(delay);
     return new Label(UUID.randomUUID().toString(), LocalDate.now());
   }
 
